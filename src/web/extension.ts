84626414de926ae0b467b0a38e2f996ff7bd137d
Dmitry Barskov <dmitry.barskov64@gmail.com>
2025-04-16 20:35:15 +0200

    Implement basic STDIN methods: read, readlines, gets, eof?

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -9,147 +9,150 @@ import { createParser, statements } from './parse';
 import initializeRubyVm from './initializeRubyVm';
 
 async function evalInVm(vm: RubyVM, source: string): Promise<{result: any, output: string | null}> {
-	console.info(`Evaluating code (${source.length} chars)`);
-	console.debug(`Source: ${source}`);
+  console.info(`Evaluating code (${source.length} chars)`);
+  console.debug(`Source: ${source}`);
 
-	try {
-		vm.eval('$captured_output = []');
-		const result = vm.eval(`(${source}).inspect`).toJS();
-		const outputPresent = vm.eval('$captured_output.any?').toJS();
-		let output: string | null = null;
-		if (outputPresent) {
-			output = vm.eval('$captured_output.join').toJS();
-		}
+  try {
+    vm.eval('$stdout.reset');
+    const result = vm.eval(`(${source}).inspect`).toJS();
+    const outputPresent = vm.eval('$stdout.any?').toJS();
+    let output: string | null = null;
+    if (outputPresent) {
+      output = vm.eval('$stdout.join').toJS();
+    }
 
-		console.info('Code evaluated successfully');
-		console.debug(`Value = ${result}, Output = ${output}`);
-		return { result, output };
-	} catch (error) {
-		if (error instanceof Error && error.message.includes("InputRequiredError")) {
-			console.info("Input required");
-			const input = await vscode.window.showInputBox();
-			console.info({ message: `Input received: ${input}`, input });
-			const stdin = vm.eval('$stdin');
-			if (input === undefined) {
-				stdin.call('ready', vm.eval('nil'));
-			} else {
-				stdin.call('ready', vm.wrap(input));
-			}
-			return evalInVm(vm, source);
-		}
-		const outputPresent = vm.eval('$captured_output.any?').toJS();
-		let output: string | null = null;
-		if (outputPresent) {
-			output = vm.eval('$captured_output.join').toJS();
-		}
-		console.error(`Error evaluating code: ${error}, Output = ${output}`);
-		return { result: error, output };
-	}
+    console.info('Code evaluated successfully');
+    console.debug(`Value = ${result}, Output = ${output}`);
+    return { result, output };
+  } catch (error) {
+    if (error instanceof Error && error.message.includes("InputRequiredError")) {
+      console.info("Input required");
+      const input = await vscode.window.showInputBox({ prompt: "Input requested", title: "STDIN" });
+      console.info({ message: `Input received: ${input}`, input });
+      const stdin = vm.eval('$stdin');
+      if (input === undefined) {
+        stdin.call('write', vm.eval('nil'));
+      } else {
+        stdin.call(
+          'write',
+          vm.eval('"' + input.replace(/\\/g, '\\\\').replace(/"/g, '\\"') + '\n"')
+        );
+      }
+      return evalInVm(vm, source);
+    }
+    const outputPresent = vm.eval('$stdout.any?').toJS();
+    let output: string | null = null;
+    if (outputPresent) {
+      output = vm.eval('$stdout.join').toJS();
+    }
+    console.error(`Error evaluating code: ${error}, Output = ${output}`);
+    return { result: error, output };
+  }
 }
 
 async function initializeWebAssmebly() {
-	console.info("Initializing WebAssembly...");
+  console.info("Initializing WebAssembly...");
 
-	console.info("Downloading Prism...");
-	const prism = await fetch("https://unpkg.com/@ruby/prism@latest/src/prism.wasm");
-	console.info("Prism downloaded successfully.");
+  console.info("Downloading Prism...");
+  const prism = await fetch("https://unpkg.com/@ruby/prism@latest/src/prism.wasm");
+  console.info("Prism downloaded successfully.");
 
-	console.info("Compiling Prism...");
-	const wasm = await WebAssembly.compileStreaming(prism);
-	console.info("Prism compiled successfully.");
+  console.info("Compiling Prism...");
+  const wasm = await WebAssembly.compileStreaming(prism);
+  console.info("Prism compiled successfully.");
 
-	console.info("Creating WASI instance...");
-	const { WASI } = await import('@bjorn3/browser_wasi_shim');
-	const wasi = new WASI([], [], []);
-	console.info("WASI instance created successfully.");
+  console.info("Creating WASI instance...");
+  const { WASI } = await import('@bjorn3/browser_wasi_shim');
+  const wasi = new WASI([], [], []);
+  console.info("WASI instance created successfully.");
 
-	console.info("Creating WebAssembly instance...");
-	const instance = await WebAssembly.instantiate(wasm, { wasi_snapshot_preview1: wasi.wasiImport });
-	wasi.initialize({
-		exports: {
-			...instance.exports,
-			memory: instance.exports["memory"] as WebAssembly.Memory,
-		},
-	});
-	console.info("WebAssembly instance created successfully.");
+  console.info("Creating WebAssembly instance...");
+  const instance = await WebAssembly.instantiate(wasm, { wasi_snapshot_preview1: wasi.wasiImport });
+  wasi.initialize({
+    exports: {
+      ...instance.exports,
+      memory: instance.exports["memory"] as WebAssembly.Memory,
+    },
+  });
+  console.info("WebAssembly instance created successfully.");
 
-	console.info("WebAssembly initialized successfully.");
-	return instance;
+  console.info("WebAssembly initialized successfully.");
+  return instance;
 }
 
 function readRubyVersion(): RUBY_VERSION {
-	return vscode.workspace.getConfiguration('ruby-eval').get('rubyVersion') as RUBY_VERSION;
+  return vscode.workspace.getConfiguration('ruby-eval').get('rubyVersion') as RUBY_VERSION;
 }
 
 export async function activate(context: vscode.ExtensionContext) {
-	let vm = await initializeRubyVm(readRubyVersion());
-	const webAssemblyInstance = await initializeWebAssmebly();
-	const parser = await createParser(webAssemblyInstance);
+  let vm = await initializeRubyVm(readRubyVersion());
+  const webAssemblyInstance = await initializeWebAssmebly();
+  const parser = await createParser(webAssemblyInstance);
 
-	let decorationType: vscode.TextEditorDecorationType | null = null;
+  let decorationType: vscode.TextEditorDecorationType | null = null;
 
-	const disposeDecorations = () => {
-		const editor: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
+  const disposeDecorations = () => {
+    const editor: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
 
-		if (editor === undefined) {
-			return;
-		}
+    if (editor === undefined) {
+      return;
+    }
 
-		if (decorationType !== null) {
-			editor.setDecorations(decorationType, []);
-			decorationType.dispose();
-		}
-		console.info("Decorations disposed");
-	};
+    if (decorationType !== null) {
+      editor.setDecorations(decorationType, []);
+      decorationType.dispose();
+    }
+    console.info("Decorations disposed");
+  };
 
-	const outputChannel = vscode.window.createOutputChannel("Ruby Output");
-	const runRuby = vscode.commands.registerCommand('ruby-eval.runRuby', async () => {
-		const editor: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
+  const outputChannel = vscode.window.createOutputChannel("Ruby Output");
+  const runRuby = vscode.commands.registerCommand('ruby-eval.runRuby', async () => {
+    const editor: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
 
-		if (editor === undefined) {
-			vscode.window.showWarningMessage("No code found");
-			return;
-		}
+    if (editor === undefined) {
+      vscode.window.showWarningMessage("No code found");
+      return;
+    }
 
-		disposeDecorations();
-		const decorations: vscode.DecorationOptions[] = [];
+    disposeDecorations();
+    const decorations: vscode.DecorationOptions[] = [];
 
-		for (const { start, end, source } of statements(parser, editor.document.getText())) {
-			const range = new vscode.Range(
-				editor.document.positionAt(start),
-				editor.document.positionAt(end)
-			);
-			const { result, output } = await evalInVm(vm, source);
-			const contentText = ` => ${result}`;
-			decorations.push({
-				range, renderOptions: {
-					after: {
-						contentText,
-						color: 'gray',
-					},
-				}
-			});
-			if (output !== null) {
-				outputChannel.append(output);
-				outputChannel.show();
-			}
-		}
+    for (const { start, end, source } of statements(parser, editor.document.getText())) {
+      const range = new vscode.Range(
+        editor.document.positionAt(start),
+        editor.document.positionAt(end)
+      );
+      const { result, output } = await evalInVm(vm, source);
+      const contentText = ` => ${result}`;
+      decorations.push({
+        range, renderOptions: {
+          after: {
+            contentText,
+            color: 'gray',
+          },
+        }
+      });
+      if (output !== null) {
+        outputChannel.append(output);
+        outputChannel.show();
+      }
+    }
 
-		decorationType = vscode.window.createTextEditorDecorationType({
-			isWholeLine: true,
-		});
-		editor.setDecorations(decorationType, decorations);
-	});
-	context.subscriptions.push(runRuby);
+    decorationType = vscode.window.createTextEditorDecorationType({
+      isWholeLine: true,
+    });
+    editor.setDecorations(decorationType, decorations);
+  });
+  context.subscriptions.push(runRuby);
 
-	const clearDecorations = vscode.commands.registerCommand('ruby-eval.clearDecorations', disposeDecorations);
-	context.subscriptions.push(clearDecorations);
+  const clearDecorations = vscode.commands.registerCommand('ruby-eval.clearDecorations', disposeDecorations);
+  context.subscriptions.push(clearDecorations);
 
-	const resetVm = vscode.commands.registerCommand('ruby-eval.resetVm', async () => {
-		disposeDecorations();
-		vm = await initializeRubyVm(readRubyVersion());
-	});
-	context.subscriptions.push(resetVm);
+  const resetVm = vscode.commands.registerCommand('ruby-eval.resetVm', async () => {
+    disposeDecorations();
+    vm = await initializeRubyVm(readRubyVersion());
+  });
+  context.subscriptions.push(resetVm);
 }
 
 export function deactivate() { }
--- src/web/initializeRubyVm.ts
+++ src/web/initializeRubyVm.ts
@@ -9,7 +9,7 @@ async function initializeRubyVm(rubyVersion: RUBY_VERSION = "3.3"): Promise<Ruby
   console.info(`Using Ruby ${rubyVersion}...`);
 
   console.info("Downloading WASM module...");
-  const stdlib = (await RUBY_VERSIONS[rubyVersion]).wasm;
+  const stdlib = await fetch(RUBY_VERSIONS[rubyVersion]);
   console.info("WASM module downloaded successfully.");
 
   console.info("Compiling WASM module...");
@@ -25,54 +25,126 @@ async function initializeRubyVm(rubyVersion: RUBY_VERSION = "3.3"): Promise<Ruby
     `
       require "js"
 
-      $captured_output = []
-      $original_stdout = $stdout
-      $stdout = Object.new.tap do |obj|
-        def obj.write(str)
-          $original_stdout.write(str)
-          $captured_output << str
+      class VscOutput
+        def initialize(original_stdout)
+          @original_stdout = original_stdout
+          @captured_output = []
+        end
+
+        def write(str)
+          $stdout.puts "Writing a #{str.class}"
+          @original_stdout.write(str)
+          @captured_output.push(str)
+        end
+
+        def any? = @captured_output.any?
+        def join = @captured_output.join
+        def reset = @captured_output = []
+
+        def respond_to_missing?(name, include_private = false)
+          @original_stdout.respond_to?(name, include_private) || super
+        end
+
+        def method_missing(name, *args)
+          if @original_stdout.respond_to?(name)
+            @original_stdout.send(name, *args)
+          else
+            super
+          end
         end
       end
+      $stdout = VscOutput.new($stdout)
 
-
-      class StdinChannel
+      class VscIO
         class InputRequiredError < StandardError; end
 
-        def ready(str)
-          @ready = true
-          @input = str
+        def initialize
+          @chars = []
+          @current_position = 0
+        end
+
+        def write(str)
+          if str.nil?
+            @chars.push(nil)
+          else
+            @chars.concat(str.chars)
+          end
         end
 
         def gets
-          if @ready
-            @ready = false
-            @input
-          else
-            raise InputRequiredError
+          if @current_position < @chars.length && @chars[@current_position].nil?
+            @current_position += 1
+            return nil
           end
+
+          end_of_line = (@current_position...@chars.length).find do |i|
+            @chars[i] == "\n" || @chars[i].nil?
+          end
+
+          require_input if end_of_line.nil?
+
+          next_line = @chars[@current_position..end_of_line].join
+          @current_position = end_of_line + 1
+          ensure_newline(next_line)
         end
 
         def read(length = nil)
-          raise NotImplementedError
+          end_of_file = @current_position
+          while end_of_file < @chars.length
+            break if length && end_of_file - @current_position >= length
+            break if @chars[end_of_file].nil?
+
+            end_of_file += 1
+          end
+          chars_read = end_of_file - @current_position
+
+          if length && chars_read >= length
+            next_chunk = @chars[@current_position...end_of_file].join
+            @current_position = end_of_file
+            return next_chunk
+          elsif end_of_file < @chars.length && @chars[end_of_file].nil?
+            next_chunk = @chars[@current_position...end_of_file].join
+            @current_position = end_of_file + 1
+            return next_chunk
+          else
+            require_input
+          end
         end
 
         def readline
-          raise NotImplementedError
+          line = gets
+          raise EOFError.new("end of file reached") if line.nil?
+          line
+        end
+
+        def readlines
+          lines = read.scan(/.*\n?/)
+          if lines.pop != ""
+            $stdout.write(
+              "Some input got truncated. Please report this behavior #{self.inspect}"
+            )
+          end
+          lines
         end
 
         def each_line(&block)
-          raise NotImplementedError
+          readlines.each(&block)
         end
 
-        def eof?
-          if @ready
-            @input.nil?
-          else
-            raise InputRequiredError
-          end
+        def eof? = false
+
+        private
+
+        def require_input
+          raise InputRequiredError.new("Input required. Call #write with a string to provide input.")
+        end
+
+        def ensure_newline(str)
+          str += "\n" if str[-1] != "\n"
+          str
         end
       end
-      $stdin = StdinChannel.new
+      $stdin = VscIO.new
     `
   );
 
